29bffbb3cb526703f5a9df259685968b8dfabf91
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-06-04 22:18:50 -0400

    Configurando Rutas de usuarios

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ conectarDB();
 // puerto de la app
 const PORT = process.env.PORT || 4000;
 
+// Importart Rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
+
 // Definir la pagina principal
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
---
+++ routes/usuarios.js
@@ -0,0 +1,9 @@
+// Rutas para crear usuarios
+const express = require('express');
+const router = express.Router();
+
+// Crea un usaurio
+// api/usuarios
+router.post('/', () => {});
+
+module.exports = router;
